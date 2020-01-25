ba2db70bb828aac6b749a20b2a1889f717b92d21
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-01-25 10:41:49 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,37 @@
+ Restaurant.destroy_all
+
+ restaurants = Restaurant.create!([
+
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'belgian'
+
+  },
+  {
+    name:         'Tasty',
+    address:      '15 rue de la paire, France 75003',
+    category:     'french'
+  },
+  {
+    name:         'Le Wok',
+    address:      '39 greenlane road, Birminngham',
+    category:     'japanese'
+  },
+  {
+    name:         'Fourchette',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian'
+  }
+])
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
